f77b562bb9fdde7763eab96856d6b5e385b6313a
CoreByte <62032154+CoreBytee@users.noreply.github.com>
2022-05-10 21:51:28 +0200

    sub members of guild

--- src/Main/lua/ga/duacord/Client/Objects/Guild.lua
+++ src/Main/lua/ga/duacord/Client/Objects/Guild.lua
@@ -1,22 +1,41 @@
 local Guild = Class:extend()
 
-local Member = Import("ga.duacord.Client.Objects.Member")
+local MemberClass = Import("ga.duacord.Client.Objects.Member")
 
-
+--#region Class methods
 function Guild:initialize(Client)
     self.Client = Client
+    self.Members = {}
+    self.Roles = {}
 end
 
 function Guild:BeforeInsert(Data)
-    p(Data)
+    Data.Region = nil
+    if Data.Members then
+        for Index, MemberData in pairs(Data.Members) do
+
+            local Member = MemberClass:new(self.Client, self)
+            self.Client.API:InsertTable(Member, MemberData)
+            self.Members[Member.User.Id] = Member
+
+        end
+        Data.Members = nil
+    end
+end
+--#endregion
+
+--#region Object Getters
+function Guild:GetMember()
+    
 end
 
-function Guild:OnInsert()
-    local Members = self.Members
-    self.Members = {}
-    for Index, Value in pairs(Members) do
-        local Member = Member:new(self.Client, self)
-    end
+function Guild:GetRole()
+    
+end
+--#endregion
+
+function Guild:SetName(Name)
+    
 end
 
 return Guild
