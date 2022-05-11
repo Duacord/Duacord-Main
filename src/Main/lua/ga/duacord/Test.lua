5b0fdab91426d0f0e093105a51af921642840a13
CoreByte <62032154+CoreBytee@users.noreply.github.com>
2022-05-11 21:55:46 +0200

    test stuff

--- src/Main/lua/ga/duacord/Test.lua
+++ src/Main/lua/ga/duacord/Test.lua
@@ -10,6 +10,9 @@ Client:On(
     "Loaded",
     function()
         p("Loaded")
-        
+        local Guild = Client:GetGuild("783625199702245436")
+        local Channel = Guild:GetChannel("783731240138047529")
+        --Channel:Send("First message from Lua")
+        print("Done")
     end
 )
