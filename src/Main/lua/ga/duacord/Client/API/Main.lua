5c813769d188c87a05ca30b2690869defa323fa6
CoreByte <62032154+CoreBytee@users.noreply.github.com>
2022-05-11 21:56:02 +0200

    api methods

--- src/Main/lua/ga/duacord/Client/API/Main.lua
+++ src/Main/lua/ga/duacord/Client/API/Main.lua
@@ -11,6 +11,11 @@ end
 
 --#region Request Methods
 local UserAgent = string.format("DiscordBot (%s, %s)", TypeWriter.LoadedPackages.duacord.Package.Contact.Source, TypeWriter.LoadedPackages.duacord.Package.Version)
+local SuccessCodes = {
+    [200] = true,
+    [201] = true,
+    [204] = true
+}
 function API:Request(Method, Endpoint, Data, Auth, Headers)
     if Headers == nil then
         Headers = {}
@@ -35,6 +40,10 @@ function API:Request(Method, Endpoint, Data, Auth, Headers)
         )
     end
     local Response, Body = Request(Method, Endpoint, ParsedHeaders, Data)
+    if SuccessCodes[Response.code] == nil then
+        TypeWriter.Logger.Error(Response.code .. " - " .. Response.reason .. " - " .. Body .. " : " .. Endpoint)
+        return
+    end
     return Response, Body
 end
 
@@ -44,6 +53,11 @@ function API:JsonRequest(Method, Endpoint, Data, Auth, Headers)
         EncodedData = Json.encode(Data)
     end
 
+    if Headers == nil then
+        Headers = {}
+    end
+    Headers["Content-Type"] = "application/json"
+
     local Response, Body = self:Request(Method, Endpoint, EncodedData, Auth, Headers)
     local Decoded
     if Body then
@@ -52,13 +66,25 @@ function API:JsonRequest(Method, Endpoint, Data, Auth, Headers)
     return Response, Decoded
 end
 
-function API:DiscordRequest(Method, Endpoint, Data, Headers, Auth)
+function API:DiscordRequest(Method, Endpoint, Data, Headers, Auth, ReturnResponse)
     if Auth == nil then
         Auth = true
     end
 
     local Response, Body = self:JsonRequest(Method, Constant.Discord.Https .. Endpoint, Data, Auth, Headers)
-    return Response, Body
+    if ReturnResponse == true then
+        return Response, Body
+    else
+        return Body
+    end
+end
+
+local function ToHex(Character)
+    return string.format('%%%02X', byte(Character))
+end
+
+function API:UrlEncode(Object)
+    return (string.gsub(tostring(Object), '%W', ToHex))
 end
 --#endregion
 
@@ -86,6 +112,174 @@ function API:InsertTable(Original, ToInsert)
     return TableInsert(Original, ToInsert)
 end
 
+--#endregion
+
+--#region Objects
+    --#region Channel Methods
+        function API:ModifyChannel(ChannelId, Data)
+            return self:DiscordRequest(
+                "PATCH",
+                string.format(
+                    "/channels/%s",
+                    Id
+                ),
+                Data
+            )            
+        end
+
+        function API:DeleteChannel(ChannelId)
+            return self:DiscordRequest(
+                "DELETE",
+                string.format(
+                    "/channels/%s",
+                    Id
+                )
+            )
+        end
+
+        function API:GetMessage(ChannelId, MessageId)
+            return self:DiscordRequest(
+                "GET",
+                string.format(
+                    "/channels/%s/messages/%s",
+                    ChannelId,
+                    MessageId
+                )
+            )
+        end
+        
+        function API:CreateMessage(ChannelId, Data)
+            return self:DiscordRequest(
+                "POST",
+                string.format(
+                    "/channels/%s/messages",
+                    ChannelId
+                ),
+                Data
+            )
+        end
+
+        -- Crosspost message 
+
+        function API:CreateReaction(ChannelId, MessageId, Emoji)
+            return self:DiscordRequest(
+                "PUT",
+                string.format(
+                    "/channels/%s/messages/%s/reactions/%s/@me",
+                    ChannelId,
+                    MessageId,
+                    self:UrlEncode(Emoji)
+                )
+            )
+        end
+
+        function API:RemoveReaction(ChannelId, MessageId, Emoji, UserId)
+            return self:DiscordRequest(
+                "DELETE",
+                string.format(
+                    "/channels/%s/messages/%s/reactions/%s/%s",
+                    ChannelId,
+                    MessageId,
+                    self:UrlEncode(Emoji),
+                    UserId or "@me"
+                )
+            )
+        end
+
+        -- Get reactions
+
+        function API:DeleteReactions(ChannelId, MessageId, Emoji)
+            local EncodedEmoji
+            if Emoji ~= nil then
+                EncodedEmoji = "/" .. self:UrlEncode(Emoji)
+            end
+            return self:DiscordRequest(
+                "DELETE",
+                string.format(
+                    "/channels/%s/messages/%s/reactions%s",
+                    ChannelId,
+                    MessageId,
+                    EncodedEmoji
+                )
+            )
+        end
+
+        function API:EditMessage(ChannelId, MessageId, Data)
+            return self:DiscordRequest(
+                "PATCH",
+                string.format(
+                    "/channels/%s/messages/%s",
+                    ChannelId,
+                    MessageId
+                ),
+                Data
+            )
+        end
+
+        function API:DeleteMessage(ChannelId, MessageId)
+            return self:DiscordRequest(
+                "DELETE",
+                string.format(
+                    "/channels/%s/messages/%s",
+                    ChannelId,
+                    MessageId
+                )
+            )
+        end
+
+        -- Bulk Delete Messages
+
+        -- Edit Channel Permissions
+
+        -- Get Channel Invites
+
+        -- Create Channel Invite
+
+        -- Delete Channel Permission
+
+        -- Follow News Channel
+
+        -- Trigger Typing Indicator
+
+        -- Get Pinned Messages
+
+        -- Pin Message
+
+        -- Unpin Message
+
+        -- Group DM Add Recipient
+
+        -- Group DM Remove Recipient
+
+        -- Start Thread from Message
+
+        -- Start Thread without Message
+
+        -- Start Thread in Forum Channel
+
+        -- Join Thread
+
+        -- Add Thread Member
+
+        -- Leave Thread
+
+        -- Remove Thread Member
+
+        -- Get Thread Member
+
+        -- List Thread Members
+
+        -- List Active Threads
+
+        -- List Public Archived Threads
+
+        -- List Private Archived Threads
+
+        -- List Joined Private Archived Threads
+
+
+
+    --#endregion
 --#endregion
 
 return API
