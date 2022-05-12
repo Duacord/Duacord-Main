b07a6ff62846d43e11b8c023998dc3cd9f7492e5
CoreByte <62032154+CoreBytee@users.noreply.github.com>
2022-05-12 22:15:37 +0200

    application command constants

---
+++ src/Main/lua/ga/duacord/Client/API/Constant/ApplicationCommands.lua
@@ -0,0 +1,16 @@
+local ApplicationCommands = {}
+
+ApplicationCommands.Option = {}
+ApplicationCommands.Option.SubCommand =             1
+ApplicationCommands.Option.SubCommandGroup =        2	
+ApplicationCommands.Option.String =                 3	
+ApplicationCommands.Option.Integer =                4
+ApplicationCommands.Option.Boolean =                5	
+ApplicationCommands.Option.User =                   6	
+ApplicationCommands.Option.Channel =                7
+ApplicationCommands.Option.Role =                   8	
+ApplicationCommands.Option.Mentionable =            9
+ApplicationCommands.Option.Number =                 10
+ApplicationCommands.Option.Attachment =             11
+
+return ApplicationCommands
