cc59f400c00fbde061bedd5c3c50a2c70640a4e4
CoreByte <62032154+CoreBytee@users.noreply.github.com>
2022-05-12 22:15:13 +0200

    channel constant

---
+++ src/Main/lua/ga/duacord/Client/API/Constant/Objects/Channel.lua
@@ -0,0 +1,17 @@
+local Channel = {}
+
+Channel.Type = {}
+Channel.Type.GuildText                 = 0
+Channel.Type.Dm                        = 1
+Channel.Type.GuildVoice                = 2
+Channel.Type.GroupDm                   = 3
+Channel.Type.GuildCategory             = 4
+Channel.Type.GuildNews                 = 5
+Channel.Type.GuildNewsThread           = 10
+Channel.Type.GuildPublicThread         = 11
+Channel.Type.GuildPrivateThread        = 12
+Channel.Type.GuildStageVoice           = 13
+Channel.Type.GuildDirectory            = 14
+Channel.Type.GuildForum                = 15
+
+return Channel
