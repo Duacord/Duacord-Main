b76331632e096e156780839865a3cdf0012dcf4c
CoreByte <62032154+CoreBytee@users.noreply.github.com>
2022-05-15 21:17:59 +0200

    constants

--- src/Main/lua/ga/duacord/Client/API/Constant/Main.lua
+++ src/Main/lua/ga/duacord/Client/API/Constant/Main.lua
@@ -4,6 +4,7 @@ Constant.ApplicationCommands         = Import("ga.duacord.Client.API.Constant.Ap
 Constant.Cdn                         = Import("ga.duacord.Client.API.Constant.Cdn")
 Constant.Discord                     = Import("ga.duacord.Client.API.Constant.Discord")
 Constant.Gateway                     = Import("ga.duacord.Client.API.Constant.Gateway")
+Constant.MessageComponents           = Import("ga.duacord.Client.API.Constant.MessageComponents")
 Constant.Websocket                   = Import("ga.duacord.Client.API.Constant.Websocket")
 
 Constant.Objects = {}
---
+++ src/Main/lua/ga/duacord/Client/API/Constant/MessageComponents.lua
@@ -0,0 +1,8 @@
+local MessageComponents = {}
+
+MessageComponents.TextInput = {}
+MessageComponents.TextInput.Style = {}
+MessageComponents.TextInput.Style.Short = 1
+MessageComponents.TextInput.Style.Paragraph = 2
+
+return MessageComponents
