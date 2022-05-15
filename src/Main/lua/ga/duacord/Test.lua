c35b22bd64db3744693521d1a6a907b171be2e2c
CoreByte <62032154+CoreBytee@users.noreply.github.com>
2022-05-15 21:19:00 +0200

    test stuff

--- src/Main/lua/ga/duacord/Test.lua
+++ src/Main/lua/ga/duacord/Test.lua
@@ -2,13 +2,27 @@ local Client = Import("ga.duacord"):new(
 
 )
 
+local TextInput = Client.Constructors.Components.TextInput()
+TextInput:SetLabel("Label"):SetTitle("Title"):SetRequired(true):SetStyle("Short"):SetId("TextInput")
+
+local TextInput2 = Client.Constructors.Components.TextInput()
+TextInput2:SetLabel("Label2"):SetTitle("Title2"):SetRequired(true):SetStyle("Paragraph"):SetId("TextInput2")
+
+
+local Modal = Client.Constructors.Modal.Modal()
+Modal:SetTitle("TestModal"):AddComponent(TextInput):AddComponent(TextInput2):SetCallback(function (Interaction)
+    --p(Interaction.Data)
+end)
+
 local Command = Client.Constructors.SlashCommands.Command()
 Command:SetName("Hello")
 Command:SetDescription("Hello command")
-Command:SetCallback(function ()
-    
+Command:SetCallback(function (Interaction)
+    Interaction:ShowModal(Modal)
 end)
 Command:Register()
+
+
 
 Client:Run(require("fs").readFileSync("Token"))
 
