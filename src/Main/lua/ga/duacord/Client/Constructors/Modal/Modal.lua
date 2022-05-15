1278f24f31024995f65ab935e180e3ed41ebc802
CoreByte <62032154+CoreBytee@users.noreply.github.com>
2022-05-15 21:15:45 +0200

    modal constructor

---
+++ src/Main/lua/ga/duacord/Client/Constructors/Modal/Modal.lua
@@ -0,0 +1,54 @@
+local Modal = Object:extend()
+
+local Constant = Import("ga.duacord.Client.API.Constant")
+
+function Modal:initialize(Client, Data)
+    Data = Data or {}
+    self.Client = Client
+    self.Callback = Data.Callback or Data.callback or function () end
+    Data.Callback = nil
+    Data.callback = nil
+
+    self.Data = Data
+    self.Data.components = {}
+    self.Data.custom_id = Client.API:GenerateCustomId()
+
+    self.Client:On(
+        "RawInteraction",
+        function (Interaction, InteractionType)
+            if InteractionType ~= Constant.ApplicationCommands.Type.ModalSubmit then return end
+            if Interaction.Data.CustomId ~= self.Data.custom_id then return end
+
+            local NewData = {}
+            for Index, Component in pairs(Interaction.Data) do
+                p(Component)
+            end
+
+            self.Callback(Interaction, self)
+        end
+    )
+end
+
+function Modal:SetTitle(Name)
+    self.Data.title = Name
+    return self
+end
+
+function Modal:AddComponent(Component)
+    if type(Component.Export) == "function" then
+        Component = Component:Export()
+    end
+    table.insert(self.Data.components, self.Client.Constructors.Components.ActionRow():AddComponent(Component):Export())
+    return self
+end
+
+function Modal:SetCallback(Fn)
+    self.Callback = Fn
+    return self
+end
+
+function Modal:Export()
+    return self.Data
+end
+
+return Modal
