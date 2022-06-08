d80a54cf466efd1c60ac27bf7eddba93012e8c40
CoreByte <62032154+CoreBytee@users.noreply.github.com>
2022-06-08 15:56:14 +0200

    Moving code

--- src/Main/lua/ga/duacord/Test.lua
+++ src/Main/lua/ga/duacord/Test.lua
@@ -2,6 +2,7 @@ local Client = Import("ga.duacord"):new(
 
 )
 
+local Modal
 do
     local TextInput = Client.Constructors.Components.TextInput()
     TextInput:SetLabel("Label"):SetTitle("Title"):SetRequired(true):SetStyle("Short"):SetId("TextInput")
@@ -10,20 +11,32 @@ do
     TextInput2:SetLabel("Label2"):SetTitle("Title2"):SetRequired(true):SetStyle("Paragraph"):SetId("TextInput2")
 
 
-    local Modal = Client.Constructors.Modal.Modal()
+    Modal = Client.Constructors.Modal.Modal()
+    Modal:SetId("TestModal")
     Modal:SetTitle("TestModal"):AddComponent(TextInput):AddComponent(TextInput2):SetCallback(function (Interaction)
-        p(Interaction.Data)
         Interaction:Reply("Label " .. Interaction.Data.TextInput.Value .. " Label2 " .. Interaction.Data.TextInput2.Value)
     end)
 end
 
-local Command = Client.Constructors.Commands.SlashCommand()
-Command:SetName("Hello")
-Command:SetDescription("Hello command")
-Command:SetCallback(function (Interaction)
-    Interaction:ShowModal(Modal)
-end)
-Command:Register()
+do
+    local Command = Client.Constructors.Commands.SlashCommand()
+    Command:SetName("Hello")
+    Command:SetDescription("Hello command")
+    Command:SetCallback(function (Interaction)
+        Interaction:ShowModal(Modal)
+    end)
+    Command:Register()
+end
+
+do
+    local UserCommand = Client.Constructors.Commands.UserCommand()
+    UserCommand:SetName("Hello")
+    UserCommand:SetCallback(function (Interaction)
+        Interaction:Reply("Hi")
+    end)
+    UserCommand:Register()
+end
+
 
 do
     local InputText = Client.Constructors.Components.TextInput()
@@ -56,18 +69,13 @@ Client:Run(require("fs").readFileSync("Token"))
 Client:On(
     "Loaded",
     function()
-        p("Loaded")
         local Guild = Client:GetGuild("783625199702245436")
         local Channel = Guild:GetChannel("783731240138047529")
         Channel:Send(
             Client.Constructors.Message.Message():SetContent("Hello World!")
             :AddComponent(
                 Client.Constructors.Components.Button():SetLabel("Hoi"):SetId("HoiButton"):SetCallback(function (Interaction)
-                    p("Hoi")
                     Interaction:Reply("Hoi")
-                    for Index, Value in pairs(Interaction) do
-                        print(Index, Value)
-                    end
                 end)
             )
         )
